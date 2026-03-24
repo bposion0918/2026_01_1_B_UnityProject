144e4fbbc9a89fdb29f8a0cb844f11d9f484f05c
bposion0918 <b.posion.0918@gmail.com>
2026-03-24 11:58:09 +0900

    03 24

--- Assets/Skripts/MyJump.cs
+++ Assets/Skripts/MyJump.cs
@@ -4,9 +4,10 @@ using UnityEngine.UI;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    public Rigidbody rigidbody;        //강체 (형태와 크기가 고정된 고체) 물리 현상이 동작하게 해주는 컴포넌트
+    public Rigidbody rb;       //강체 (형태와 크기가 고정된 고체) 물리 현상이 동작하게 해주는 컴포넌트
     public float power = 20f;   //변수 힘을 선언 함
     public Text TextUI;
+    public float timer = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -23,7 +24,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
         if (Input.GetKeyUp(KeyCode.Space))                   //스페이스 키를 눌렀을때
         {
             power = power + Random.Range(-100, 200);      //Power를 랜덤으로 변경 시킨다. (-100, 200)사이의 값을 더한다.
-            rigidbody.AddForce(transform.up * power);     //변수(power)의 위쪽으로 힘을 준다
+            rb.AddForce(transform.up * power);     //변수(power)의 위쪽으로 힘을 준다
 
         }
 
